b080a53331cde1763ee038b8122820b90a4ca6e1
Alena T <aaldoshina@ostrovok.ru>
2021-09-15 22:50:18 +0300

    optimize

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -2,6 +2,7 @@ import django.urls
 from django.contrib import admin
 
 urlpatterns = [
-    django.urls.path('', django.urls.include('infra_app.urls', namespace='infra_app')),
+    django.urls.path('', django.urls.include(
+        'infra_app.urls', namespace='infra_app')),
     django.urls.path('admin/', admin.site.urls),
 ]
